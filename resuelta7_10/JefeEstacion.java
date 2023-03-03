111f1f2cb682aa5aea5ee85cefcdd9824772c126
diabroblanco <123973655+diabroblanco@users.noreply.github.com>
2023-03-03 13:59:47 +0100

    Add files via upload

---
+++ resuelta7_10/JefeEstacion.java
@@ -0,0 +1,16 @@
+package resuelta7_10;
+ import java.time.LocalDateTime;
+public class JefeEstacion {
+
+	String nombre;
+	String dni;
+	LocalDateTime nombramiento;
+	
+	public JefeEstacion(String nombre, String dni, LocalDateTime nombramiento) {
+		this.nombre=nombre;
+		this.dni=dni;
+		this.nombramiento=nombramiento;
+	}
+	
+	
+}
---
+++ resuelta7_10/Mecanico.java
@@ -0,0 +1,18 @@
+package resuelta7_10;
+
+public class Mecanico {
+	String nombre;
+	String telefono;
+	enum Especialidad{FRENOS, HIDRAULICA, ELECTRICIDAD, MOTOR}
+	Especialidad especialidad;
+	
+	
+	
+	public Mecanico(String nombre, String telefono, String especialidad) {
+	this.nombre=nombre;
+	this.telefono=telefono;
+	this.especialidad=Especialidad.valueOf(especialidad);
+		
+		
+	}
+}
